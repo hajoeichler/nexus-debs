55739c111acc96c0087075932c5b429558680593
Hajo Eichler <hajo.eichler@commercetools.de>
2012-05-03 19:28:41 +0000

    Made update version script a bit nicer.

--- update-version.sh
+++ update-version.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-NEXUS_VERSION=2.0.4
+usage() {
+  echo "$(basename $0) VERSION: Updates the version in all files."
+}
+
+update_version() {
+  sed -i -e "s#NEXUS_VERSION=.*#NEXUS_VERSION=\"${NEXUS_VERSION}\"#g" download-nexus.sh
+  sed -i -e "s#NEXUS_VERSION := .*#NEXUS_VERSION := \"${NEXUS_VERSION}\"#g" nexus/Makefile
+  sed -i -e "s#nexus .*#nexus (${NEXUS_VERSION}) unstable; urgency=low#g" nexus/debian/changelog
+}
 
 set -e
 
-sed -i -e "s#NEXUS_VERSION=.*#NEXUS_VERSION=\"${NEXUS_VERSION}\"#g" download-nexus.sh
-sed -i -e "s#NEXUS_VERSION := .*#NEXUS_VERSION := \"${NEXUS_VERSION}\"#g" nexus/Makefile
-sed -i -e "s#nexus .*#nexus (${NEXUS_VERSION}) unstable; urgency=low#g" nexus/debian/changelog
+if [ -z $1 ]; then
+  usage
+  exit 1;
+fi
+
+
+readonly NEXUS_VERSION=$1
+update_version
